7c9c1d88e894c4bcfd115a26759afda4d870b25a
stona1 <stona1@gmail.com>
2021-01-30 16:22:33 +0100

    Uruchomienie testowej wysyłki wiadomość z formularza

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'stona1@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formularz kontaktowy ze strony MedAndMore:  $name";
+$email_body = "Wiadomość z formularza kontaktowego.\n\n"."Poniżej zawarte są szczegóły wiadomości:\n\nImię i nazwisko: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
+$headers = "From: as@esluch.pl\n"; // This is the email address the generated message will be from. We recommend using something like noWiadomośćourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
