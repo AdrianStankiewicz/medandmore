bf76e45861c26e0787ed647c29af878872941e8b
stona1 <stona1@gmail.com>
2021-02-04 17:27:12 +0100

    Update email address for contact form

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'stona1@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'office@medandmore.pl'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formularz kontaktowy ze strony MedAndMore:  $name";
 $email_body = "Wiadomość z formularza kontaktowego.\n\n"."Poniżej zawarte są szczegóły wiadomości:\n\nImię i nazwisko: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
-$headers = "From: as@esluch.pl\n"; // This is the email address the generated message will be from. We recommend using something like noWiadomośćourdomain.com.
+$headers = "From: office@medandmore.pl\n"; // This is the email address the generated message will be from. We recommend using something like noWiadomośćourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
