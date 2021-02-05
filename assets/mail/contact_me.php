4de7cd667553c2d3bbec755285ec85b92c6bf9ec
stona1 <stona1@gmail.com>
2021-02-05 10:34:58 +0100

    Added missing $to in contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-
+$to = 'office@medandmore.pl'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formularz kontaktowy ze strony MedAndMore:  $name";
 $email_body = "Wiadomość z formularza kontaktowego.\n\n"."Poniżej zawarte są szczegóły wiadomości:\n\nImię i nazwisko: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
 $headers = "From: office@medandmore.pl\n"; // This is the email address the generated message will be from. We recommend using something like noWiadomośćourdomain.com.
